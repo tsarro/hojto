308dc23737038c4fc5efb1907a0eb9711df87523
unknown <saarinensamueli@gmail.com>
2018-03-13 16:53:32 +0200

    linkit kahteen ekaa kategoriaan

--- src/main/java/fi/academy/hojto/ForumController.java
+++ src/main/java/fi/academy/hojto/ForumController.java
@@ -14,9 +14,12 @@ import java.util.Optional;
 @Controller
 public class ForumController {
 
-    @Autowired MessageRepository mrepo;
-    @Autowired TopicRepository trepo;
-    @Autowired UserRepository urepo;
+    @Autowired
+    MessageRepository mrepo;
+    @Autowired
+    TopicRepository trepo;
+    @Autowired
+    UserRepository urepo;
 
     //haetaan kaikki topicit @Juuso
     /*@GetMapping("/topics")
@@ -25,14 +28,29 @@ public class ForumController {
         return "topic";
     }*/
 
-    // Haetaan kaikki topicit tietystä kategoriasta @Olli,Outi,Heidi,Juuso
-    @GetMapping("/topics")
-    public String specificTopic (Model model){
-        model.addAttribute("topiclist", trepo.findByCategoryId(2));
+    @GetMapping("/frontpage")
+    public String jotain(Model model){
+        return "frontpage";
+    }
+
+    @GetMapping("/topics1")
+    public String specificTopic1(Model model) {
+        model.addAttribute("topiclist", trepo.findByCategoryId(1));
         return "topic";
     }
 
 
-
-
+    // Haetaan kaikki topicit tietystä kategoriasta @Olli,Outi,Heidi,Juuso
+    @GetMapping("/topics2")
+    public String specificTopic2(Model model) {
+        model.addAttribute("topiclist", trepo.findByCategoryId(2));
+        return "topic";
+    }
 }
+
+
+
+
+
+
+
