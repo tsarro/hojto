7691a237c11abbc9911627613121a491ad723884
outiarimaa <outi.arimaa@gmail.com>
2018-03-13 11:59:03 +0200

    tietokanta luotu

--- src/main/java/fi/academy/hojto/HojtoApplication.java
+++ src/main/java/fi/academy/hojto/HojtoApplication.java
@@ -1,7 +1,9 @@
 package fi.academy.hojto;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HojtoApplication {
@@ -9,4 +11,20 @@ public class HojtoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HojtoApplication.class, args);
 	}
+
+	@Bean
+	CommandLineRunner luoTestiKayttaja(UserRepository userRepository) {
+		return args -> {
+
+			luontiYksittain(userRepository);
+		};
+
+	}
+
+	protected void luontiYksittain(UserRepository userRepository) {
+		User u = new User();
+		u.setName("Maija");
+		userRepository.save(u);
+	}
+
 }
--- src/main/java/fi/academy/hojto/MessageRepository.java
+++ src/main/java/fi/academy/hojto/MessageRepository.java
@@ -1,6 +1,8 @@
 package fi.academy.hojto;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.repository.CrudRepository;
 
 public interface MessageRepository extends CrudRepository<Message, Integer> {
+
 }
