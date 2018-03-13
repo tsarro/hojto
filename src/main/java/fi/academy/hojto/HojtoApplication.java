5453f8e25e20c3c2a681db4effcbd4051d938249
gintonic <toni.sarro@gmail.com>
2018-03-13 10:51:48 +0200

    JUUKELI

---
+++ src/main/java/fi/academy/hojto/HojtoApplication.java
@@ -0,0 +1,12 @@
+package fi.academy.hojto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HojtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HojtoApplication.class, args);
+	}
+}
