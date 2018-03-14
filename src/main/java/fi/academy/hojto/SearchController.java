60861f1d4a14bf1482c0a9989d026f88d973d038
pumpatta <jliljavirta@gmail.com>
2018-03-14 13:29:11 +0200

    uusi controller

---
+++ src/main/java/fi/academy/hojto/SearchController.java
@@ -0,0 +1,32 @@
+package fi.academy.hojto;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class SearchController {
+
+    @Autowired
+    MessageRepository mrepo;
+    @Autowired
+    TopicRepository trepo;
+    @Autowired
+    UserRepository urepo;
+
+    @GetMapping("/search")
+    public String jotain(Model model) {
+        return "seach";
+    }
+
+    @PostMapping("/searchResult")
+    public String searchMessageContainingWord(Message message, Model model) {
+        List<Message> ce = mrepo.findByMessageContains(message.getMessage());
+        model.addAttribute("messages", ce);
+        return "posts";
+    }
+}
