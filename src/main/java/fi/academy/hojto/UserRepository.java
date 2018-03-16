2145cf13da2fc6fece49a611c5e28315e8aa1328
gintonic <toni.sarro@gmail.com>
2018-03-16 09:19:12 +0200

    siivottu turhia kommentteja

--- src/main/java/fi/academy/hojto/UserRepository.java
+++ src/main/java/fi/academy/hojto/UserRepository.java
@@ -2,9 +2,6 @@ package fi.academy.hojto;
 
 import org.springframework.data.repository.CrudRepository;
 
-import org.springframework.stereotype.Repository;
-import fi.academy.hojto.User;
-
 public interface UserRepository extends CrudRepository<User, Long> {
 
 
